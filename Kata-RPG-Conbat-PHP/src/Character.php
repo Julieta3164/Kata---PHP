894c2e7c6aec34a7ca4a9198e05ef8623ba81130
Juliet Linarez <julietlinarez@Mac-mini-de-Heber.local>
2022-08-23 08:36:37 +0200

    funcion

--- Kata-RPG-Conbat-PHP/src/Character.php
+++ Kata-RPG-Conbat-PHP/src/Character.php
@@ -2,7 +2,21 @@
 
 namespace App;
 
+use phpDocumentor\Reflection\Type\This;
+
 class Character {
+    private int $helth;
+    private int $level;
+    private bool $live;
+
+    public function _construct()
+    {
+        $this->helth = 1000;
+        $this->level = 1;
+        $this->live = true;
+    }
+
+        
 
     
     
